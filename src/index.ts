05dc2d40ad9c3984356b92bc6799f32d3a2f346f
Diogo Ferreira <ddiogof20@gmail.com>
2023-06-02 16:07:23 -0300

    liberação CORS adicionada

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", process.env.URL);
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.listen(porta, () => {
     console.log(`O servidor está ouvindo na porta ${porta}.`)
 });
